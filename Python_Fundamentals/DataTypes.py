eb0307a2d5ca539716db5d99cbce3c589105670f
yemu-hub <yemiwed@gmail.com>
2024-03-09 22:27:59 -0800

    Data Types

---
+++ Python_Fundamentals/DataTypes.py
@@ -0,0 +1,22 @@
+#Numbers
+
+#numbers with thier operatons
+print(123 + 222) 
+print(2*3)
+print(2 **3)
+
+#Strings
+#Strings and thier operations
+S = 'Spam'
+print(len(S))
+
+# fetch elements using indexing expressions
+  #forward indexing
+print(S[0])
+print(S[1])
+  #backward indexing
+print(S[-1])
+print(S[-2])
+
+
+
