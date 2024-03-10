dac3aa90bf3f0497c020a0f5aed741b35fddee51
yemu-hub <yemiwed@gmail.com>
2024-03-10 07:33:13 -0700

    Variables and Basic Expressions

---
+++ Python_Fundamentals/Variables.py
@@ -0,0 +1,19 @@
+# Creating variable of type Number
+a = 3
+b = 4
+
+# Using objects in expressions
+print(a+1 , a-1)
+print(b*3, b/2)
+print(a%2, b**2)
+print(2 + 4.0, 2.0**b)
+# print(c*2) #raise error, variable 'c' not assigned value
+
+# Numeric Display Formats
+print(b/(2.0 + a)) #print round off digits
+print(1/2.0)
+
+num = 1/3.0
+print(num)
+print('%e' % num) # String formatting expression
+print('%4.2f' % num) # Floating point format
